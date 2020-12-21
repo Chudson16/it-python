63e0552b62a3e3cc5e1ff6e152a71be141e2f193
Chudson16 <clay.daniel.hudson@gmail.com>
2020-12-21 09:32:43 -0500

    Adds questions to helloworld

--- helloworld.py
+++ helloworld.py
@@ -6,7 +6,19 @@ print("| By Clay     |")
 print("+-------------+")
 print('')
 
-print("Hello World!")
+name = input('What is your name? ')
 
+age = input("What is your age? ")
+
+if age.isnumeric():
+    age = int(age)
+
+
+    print(f"Hello {name}. You are {age} years old")
+
+    print(f"Next year, you will be {age + 1} years old.")
+
+else:
+    print("You didn't type a valid number...")
 
 
