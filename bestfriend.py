04ebc55d0ed63c7d4df3e0f79cc9b519b6053459
Chudson16 <clay.daniel.hudson@gmail.com>
2020-12-21 10:20:42 -0500

    adds go agains

--- bestfriend.py
+++ bestfriend.py
@@ -6,20 +6,28 @@ print("| By Clay           |")
 print("+-------------------+")
 print('')
 
-print("Hi. I'm the computer.")
-name = input("What's your name? ")
+while True:
+
+    print("Hi. I'm the computer.")
+    name = input("What's your name? ")
 
 
-color = input(f"Hi, {name}! What's your favorite color? ")
-print(f"No way! My favorite color is also {color}!")
+    color = input(f"Hi, {name}! What's your favorite color? ")
+    print(f"No way! My favorite color is also {color}!")
 
 
-movie = input("What's your favorite movie? ")
-print(f"What!?! My favorite movie is also {movie}!")
+    movie = input("What's your favorite movie? ")
+    print(f"What!?! My favorite movie is also {movie}!")
 
 
-game = input("What is your favorite game? ")
-print(f"Get out of here! My favorite game is {game}, too!")
+    game = input("What is your favorite game? ")
+    print(f"Get out of here! My favorite game is {game}, too!")
 
 
-print("We like the same things! We should be best friends!")
+    print("We like the same things! We should be best friends!")
+
+    print("")
+
+    response = input("Would you like to go again? (Y/n) ")
+    if response.lower() in ["n", "no", "nope", "nein"]:
+        break
